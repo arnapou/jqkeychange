094c75d347d421c3ec91ddc8a1d2208ec120a483
Arnaud Buathier <arnaud@arnapou.net>
2015-02-13 12:47:24 +0100

    update to manage dynamic dom change

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ Key change jQuery plugin
 
 Features
 ========
+
 * trigger a jquery 'keyChange' event : it is an event which is between keypress and change event on an input or textarea.
 
+[Click here to see the demo](http://arnapou.net/tech-js/jquery-keychange/)
--- src/keyChange.js
+++ src/keyChange.js
@@ -1,9 +1,19 @@
-(function($) {
-	$.fn.keyChange = function(options, callback) {
+(function ($) {
+	$.fn.keyChange = function (options, selector, callback) {
 
-		if($.isFunction(options)) {
+		if ($.isFunction(options)) {
 			callback = options;
 			options = {};
+			selector = undefined;
+		}
+		if ($.isFunction(selector)) {
+			callback = selector;
+			if (typeof (options) === 'string') {
+				selector = options;
+				options = {};
+			} else {
+				selector = undefined;
+			}
 		}
 
 		options = $.extend({
@@ -11,27 +21,51 @@
 			delay: 300
 		}, options || {});
 
-		return this.each(function() {
-			var $input = $(this);
-			if(!$input.is('input') && !$input.is('textarea')) return;
-			if($.isFunction(callback)) {
-				$input.bind('keyChange', callback);
+		function init(val) {
+			var $this = $(this);
+			if (!$this.data('kcInitialized')) {
+				if ($this.is('input') || $this.is('textarea')) {
+					$this.data('kcText', typeof (val) === 'undefined' ? String($this.val()) : val);
+					$this.data('kcFunction', null);
+				}
+				$this.data('kcInitialized', true);
 			}
-			if(!$input.data('keyChangeBindingDone')) {
-				var text = String($input.val());
-				var search = null;
-				$input.keyup(function() {
-					var val = $input.val();
-					if(text != val && val.length >= options.minLength) {
-						text = val;
-						search && clearTimeout(search);
-						search = setTimeout(function() {
-							$input.trigger('keyChange', val);
-						}, options.delay);
-					}
-				});
-				$input.data('keyChangeBindingDone', true);
+		}
+
+		function keyup() {
+			init.call(this, '');
+			var $this = $(this);
+			if ($this.data('kcInitialized')) {
+				var val = String($this.val());
+				var text = $this.data('kcText');
+				var delayedFunction = $this.data('kcFunction');
+				if (text !== val && val.length >= options.minLength) {
+					$this.data('kcText', val);
+					delayedFunction && clearTimeout(delayedFunction);
+					$this.data('kcFunction', setTimeout(function () {
+						$this.trigger('keyChange', val);
+					}, options.delay));
+				}
 			}
-		});
+		}
+
+		if (typeof (selector) === 'string') {
+			this.on('change', selector, keyup);
+			this.on('keyup', selector, keyup);
+			if ($.isFunction(callback)) {
+				this.on('keyChange', selector, callback);
+			}
+		} else {
+			this.each(function () {
+				init.apply(this);
+			});
+			this.change(keyup);
+			this.keyup(keyup);
+			if ($.isFunction(callback)) {
+				this.bind('keyChange', callback);
+			}
+		}
+		return this;
 	};
 })(jQuery);
+
