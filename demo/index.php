a58efbcf423c0b9aea34126278ca0fb449dbb718
Arnaud Buathier <arnaud@buathier.me>
2019-12-22 18:24:40 +0100

    bootstrap change cdn domain

--- demo/index.php
+++ demo/index.php
@@ -25,10 +25,10 @@ function FC($file) {
 <html>
     <head>
         <title>jqKeychange</title>
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="//stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="style.css">
         <script src="//code.jquery.com/jquery-1.10.1.min.js"></script>
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="//stackpath.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <script src="../src/keyChange.js"></script>
     </head>
     <body>
